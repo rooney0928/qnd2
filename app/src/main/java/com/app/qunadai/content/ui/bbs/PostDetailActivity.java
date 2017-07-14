5eb801b4e6ce243f862fead0b897e152c87d4023
rooney0928 <rooney0928@hotmail.com>
2017-07-14 17:53:03 +0800

    4.2

--- app/src/main/java/com/app/qunadai/content/ui/bbs/PostDetailActivity.java
+++ app/src/main/java/com/app/qunadai/content/ui/bbs/PostDetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Rect;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -12,6 +13,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.CheckBox;
@@ -116,6 +118,9 @@ public class PostDetailActivity extends BaseActivity implements PostDetailContra
     @BindView(R.id.tv_comment)
     TextView tv_comment;
 
+    @BindView(R.id.view_comment)
+    View view_comment;
+
     @BindView(R.id.rv_pics)
     RecyclerView rv_pics;
 
@@ -213,6 +218,8 @@ public class PostDetailActivity extends BaseActivity implements PostDetailContra
         token = PrefUtil.getString(this, PrefKey.TOKEN, "");
 
 
+        View root = getWindow().getDecorView();
+        controlKeyboardLayout(root,et_comment);
     }
 
     /**
@@ -614,6 +621,42 @@ public class PostDetailActivity extends BaseActivity implements PostDetailContra
         }
 
 
+    }
+
+    /**
+     * 解决在页面底部置输入框，输入法弹出遮挡部分输入框的问题
+     * @param root 页面根元素
+     * @param editLayout 被软键盘遮挡的输入框
+     */
+    public static void controlKeyboardLayout(final View root,
+                                             final View editLayout) {
+        // TODO Auto-generated method stub
+        root.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+
+            @Override
+            public void onGlobalLayout() {
+                // TODO Auto-generated method stub
+                Rect rect=new Rect();
+                //获取root在窗体的可视区域
+                root.getWindowVisibleDisplayFrame(rect);
+                //获取root在窗体的不可视区域高度(被其他View遮挡的区域高度)
+                int rootInVisibleHeight=root.getRootView().getHeight()-rect.bottom;
+                //若不可视区域高度大于100，则键盘显示
+                if (rootInVisibleHeight > 100) {
+                    LogU.t( "不可视区域高度大于100，则键盘显示");
+                    int[] location = new int[2];
+                    //获取editLayout在窗体的坐标
+                    editLayout.getLocationInWindow(location);
+                    //计算root滚动高度，使editLayout在可见区域
+                    int srollHeight = (location[1] + editLayout.getHeight()) - rect.bottom;
+                    root.scrollTo(0, srollHeight);
+                } else {
+                    //键盘隐藏
+                    LogU.t("不可视区域高度小于100，则键盘隐藏");
+                    root.scrollTo(0, 0);
+                }
+            }
+        });
     }
 
     @Override
