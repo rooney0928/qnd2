3f002508235fc351f59e5bd42c702073c9966155
rooney0928 <rooney0928@hotmail.com>
2017-08-14 17:36:43 +0800

    流程更改，验证码bug

--- app/src/main/java/com/app/qunadai/content/ui/user/RegisterActivity.java
+++ app/src/main/java/com/app/qunadai/content/ui/user/RegisterActivity.java
@@ -137,8 +137,6 @@ public class RegisterActivity extends BaseActivity implements RegisterContract.V
                     }
                     //请求发送短信
                     registerPresenter.requestRegisterSms(phone);
-                    et_register_sms_pic.setText("");
-                    setPicCode();
                 }
 
 
