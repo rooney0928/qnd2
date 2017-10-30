9d43f5fc9048430bc329e27905c6d52e653fe7fc
luo <lyc549527@gmail.com>
2017-10-30 16:42:41 +0800

    5.1.0

--- app/src/main/java/com/app/qunadai/utils/ImgUtil.java
+++ app/src/main/java/com/app/qunadai/utils/ImgUtil.java
@@ -4,8 +4,8 @@ import android.content.Context;
 import android.widget.ImageView;
 
 import com.app.qunadai.R;
-import com.app.qunadai.third.GlideCircleTransform;
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 
 /**
  * Created by wayne on 2017/5/10.
@@ -13,33 +13,55 @@ import com.bumptech.glide.Glide;
 
 public class ImgUtil {
 
+    private static final int ERROR_IMG = R.mipmap.moren;
+
     public static void loadRound(Context context, int id, ImageView iv) {
-        Glide.with(context).load(id).error(R.mipmap.moren).transform(new GlideCircleTransform(context)).into(iv);
+        Glide.with(context).load(id)
+                .apply(new RequestOptions().circleCrop().error(ERROR_IMG))
+                .into(iv);
+
     }
 
     public static void loadRound(Context context, String url, ImageView iv) {
-        Glide.with(context).load(url).error(R.mipmap.moren).transform(new GlideCircleTransform(context)).into(iv);
+        Glide.with(context).load(url)
+                .apply(new RequestOptions().circleCrop().error(ERROR_IMG))
+                .into(iv);
     }
 
     public static void loadImgAvatar(Context context, int id, ImageView iv) {
-        Glide.with(context).load(id).error(R.mipmap.moren).transform(new GlideCircleTransform(context)).into(iv);
+        Glide.with(context).load(id)
+                .apply(new RequestOptions().circleCrop().error(ERROR_IMG))
+                .into(iv);
     }
 
     public static void loadImgAvatar(Context context, String url, ImageView iv) {
-        Glide.with(context).load(url).error(R.mipmap.moren).transform(new GlideCircleTransform(context)).into(iv);
+        Glide.with(context).load(url)
+                .apply(new RequestOptions().circleCrop().error(ERROR_IMG))
+                .into(iv);
     }
 
     public static void loadImg(Context context, String url, ImageView iv) {
-        Glide.with(context).load(url).error(R.mipmap.moren).into(iv);
+        Glide.with(context).load(url)
+                .apply(new RequestOptions().error(ERROR_IMG))
+                .into(iv);
     }
 
     public static void loadImg(Context context, int id, ImageView iv) {
-        Glide.with(context).load(id).error(R.mipmap.moren).into(iv);
+        Glide.with(context).load(id)
+                .apply(new RequestOptions().error(ERROR_IMG))
+                .into(iv);
     }
+
     public static void loadBanner(Context context, int id, ImageView iv) {
-        Glide.with(context).load(id).centerCrop().error(R.mipmap.moren).into(iv);
+        Glide.with(context).load(id)
+                .apply(new RequestOptions().error(ERROR_IMG))
+                .into(iv);
     }
-    public static void loadBanner(Context context,  String url, ImageView iv) {
-        Glide.with(context).load(url).centerCrop().error(R.mipmap.moren).into(iv);
+
+    public static void loadBanner(Context context, String url, ImageView iv) {
+        Glide.with(context).load(url)
+                .apply(new RequestOptions().error(ERROR_IMG))
+                .into(iv);
+
     }
 }
